94ecefe0747bdc3ee9243abfebfeb1c5010af14b
Fierce-Black-Dragon <blackdragon4204@gmail.com>
2022-07-26 21:57:31 +0530

    bug fix

--- src/app/api/apiScile.js
+++ src/app/api/apiScile.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setCredentials, logOut } from "../../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:4001/api",
+  baseUrl: "https://restapinew.herokuapp.com/api",
   credentials: "include",
 
   prepareHeaders: (headers, { getState }) => {
